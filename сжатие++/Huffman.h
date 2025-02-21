f9e2ec1a02777a20a13b4ced8af75c51b9380e30
Maria_K <anansy.pineapple@gmail.com>
2025-02-21 03:29:44 +0300

    обработка запроса, добавление функций, создание дата-файла

--- сжатие++/Huffman.h
+++ сжатие++/Huffman.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #define SIZE 256
+#define BIT8 8
 using namespace std;
 
 struct Node
@@ -22,3 +23,7 @@ Node* createNode(unsigned char symb, unsigned int freq, Node* left, Node* right)
 Node* createTree(const int freq[]);
 void encode(Node* root, string str, string sCodes[]);
 string encode_string(const vector<unsigned char>& givenStr, const string sCodes[]);
+string bit_string2encode_text(string encode_string_,int& tail_);
+string symb_bit(int lenght_, unsigned symb_);
+string encode_text2bit_string(string encode_text_);
+string bit_string2decode_text(string bit_string_, Node* root_);
