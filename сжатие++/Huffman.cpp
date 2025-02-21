80d943a4ae3dd3559a1126821851fb2603e85db1
Maria_K <anansy.pineapple@gmail.com>
2025-02-21 09:01:40 +0300

    финальный файл!

--- сжатие++/Huffman.cpp
+++ сжатие++/Huffman.cpp
@@ -4,148 +4,149 @@
 #include <vector>
 #include <queue>
 #include "Huffman.h"
+
 using namespace std;
 
 Node* createNode(unsigned char symb, unsigned int freq, Node* left, Node* right)
 {
-	Node* node = new Node();
-	node->symb = symb;
-	node->freq = freq;
-	node->left = left;
-	node->right = right;
+    Node* node = new Node();
+    node->symb = symb;
+    node->freq = freq;
+    node->left = left;
+    node->right = right;
 
-	return node;
+    return node;
 }
 
 Node* createTree(const int freq[])
 {
-	//создание очереди с приоритетом
-	priority_queue<Node*, vector<Node*>, comp> pq;
-	for (int i = 0; i < SIZE; i++)
-	{
-		if (freq[i] != 0)
-		{
-			Node* newNode = createNode(char(i), freq[i], nullptr, nullptr);
-			pq.push(newNode); //кладем ноду в очередь
-		}
-	}
-	//создание дерева на основе очереди
-	while (pq.size() != 1)
-	{
-		Node* left = pq.top();
-		pq.pop();
-		Node* right = pq.top();
-		pq.pop();
-		int false_node_sum = left->freq + right->freq;
-		Node* newFalseNode = createNode('\0', false_node_sum, left, right);
-		pq.push(newFalseNode);
-	}
-	Node* root = pq.top();
-	return root;
+    //создание очереди с приоритетом
+    priority_queue<Node*, vector<Node*>, comp> pq;
+    for (int i = 0; i < SIZE; i++)
+    {
+        if (freq[i] != 0)
+        {
+            //cout << unsigned char(i) << ' ' << freq[i] << endl;
+            Node* newNode = createNode(char(i), freq[i], nullptr, nullptr);
+            pq.push(newNode); //кладем ноду в очередь
+        }
+    }
+    //создание дерева на основе очереди
+    while (pq.size() != 1)
+    {
+        Node* left = pq.top();
+        pq.pop();
+        Node* right = pq.top();
+        pq.pop();
+        int false_node_sum = left->freq + right->freq;
+        Node* newFalseNode = createNode('\0', false_node_sum, left, right);
+        pq.push(newFalseNode);
+    }
+    Node* root = pq.top();
+    return root;
 }
 
 //функция кодировки
 void encode(Node* root, string str, string sCodes[])
 {
-	if (root == nullptr)
-		return;
+    if (root == nullptr)
+        return;
 
-	if (!root->left && !root->right)
-	{
-		sCodes[root->symb] = str;
-		return;
-	}
-	encode(root->left, str + '0', sCodes);
-	encode(root->right, str + '1', sCodes);
+    if (!root->left && !root->right)
+    {
+        sCodes[root->symb] = str;
+        return;
+    }
+    encode(root->left, str + '0', sCodes);
+    encode(root->right, str + '1', sCodes);
 }
 
 //сюда передается исходная строка и массив кодов
 string encode_string(const vector<unsigned char>& givenStr, const string sCodes[])
 {
-	string encodedStr = ""; //исходная строка в виде кодов
-	for (int i = 0; i < givenStr.size(); ++i)
-	{
-		unsigned char byte = givenStr[i];
-		string code = sCodes[byte];
-		if (!code.empty())
-		{
-			encodedStr += code;
-		}
-	}
-	return encodedStr;
+    string encodedStr = ""; //исходная строка в виде кодов
+    for (int i = 0; i < givenStr.size(); ++i)
+    {
+        unsigned char byte = givenStr[i];
+        string code = sCodes[byte];
+        if (!code.empty())
+        {
+            encodedStr += code;
+        }
+    }
+    return encodedStr;
 }
 
 string bit_string2encode_text(string encode_string_, int& tail_)
 {
-	string encode_text = "";
-	int lenght = encode_string_.size();
-	tail_ = lenght % BIT8;
-	char symbol = 0;
-	for (int i = 0; i < lenght; i++) 
-	{
-		symbol <<= 1;
-		symbol += encode_string_[i] - '0';
-		if (i % BIT8 == BIT8 - 1) 
-		{
-			encode_text += symbol;
-			symbol = 0;
-		}
-	}
-	encode_text += symbol;
-	return encode_text;
+    string encode_text = "";
+    int lenght = encode_string_.size();
+    tail_ = lenght % BIT8;
+    char symbol = 0;
+    for (int i = 0; i < lenght; i++)
+    {
+        symbol <<= 1;
+        symbol += encode_string_[i] - '0';
+        if (i % BIT8 == BIT8 - 1)
+        {
+            encode_text += symbol;
+            symbol = 0;
+        }
+    }
+    encode_text += symbol;
+    return encode_text;
 }
 
 //для создания битовой строчки символа из encode.text
 string symb_bit(int lenght_, unsigned symb_)
 {
-	string bit(lenght_, ' ');
-	for (int i = 0; i < lenght_; i++)
-	{
-		bit[lenght_ - 1 - i] = '0' + symb_ % 2;
-		symb_ /= 2;
-	}
-	return bit;
+    string bit(lenght_, ' ');
+    for (int i = 0; i < lenght_; i++)
+    {
+        bit[lenght_ - 1 - i] = '0' + symb_ % 2;
+        symb_ /= 2;
+    }
+    return bit;
 }
 
-string encode_text2bit_string(string encode_text_)
+string encode_text2bit_string(string encode_text_, int tail_)
 {
-	string bit_string = "";
-	vector <string> bits;
-	int tail = encode_text_[0];
-	int lenght = encode_text_.size();
-	for (int i = 1; i < lenght - 1; i++)
-	{
-		unsigned c = encode_text_[i];
-		if (bits[c] == "")
-		{
-			bits[c] = symb_bit(BIT8, c);
-		}
-		bit_string += bits[c];
-	}
-	bit_string = bit_string + symb_bit(tail, encode_text_[lenght - 1]);
-	return bit_string;
+    string bit_string = "";
+    string bits[SIZE];
+    int lenght = encode_text_.size();
+    for (int i = 0; i < lenght - 1; i++)
+    {
+        unsigned char c = encode_text_[i];
+        if (bits[c] == "")
+        {
+            bits[c] = symb_bit(BIT8, c);
+        }
+        bit_string += bits[c];
+    }
+    bit_string = bit_string + symb_bit(tail_, encode_text_[lenght - 1]);
+    return bit_string;
 }
 
 string bit_string2decode_text(string bit_string_, Node* root_)
 {
-	Node* a = root_;
-	string decode_text;
-	for (int i = 0; i < bit_string_.size(); i++)
-	{
-		if (bit_string_[i] == '0')
-		{
-			root_ = root_->left;
-		}
-		else
-		{
-			root_ = root_->right;
-		}
-		if (!root_->left && !root_->right)
-		{
-			decode_text += root_->symb;
-			root_ = a;
-		}
+    Node* root = root_;
+    string decode_text;
+    for (int i = 0; i < bit_string_.size(); i++)
+    {
+        if (bit_string_[i] == '0')
+        {
+            root_ = root_->left;
+        }
+        else
+        {
+            root_ = root_->right;
+        }
+        if (!root_->left && !root_->right)
+        {
+            decode_text += root_->symb;
+            root_ = root;
+        }
 
-	}
-	return decode_text;
+    }
+    return decode_text;
 }
--- сжатие++/Huffman.h
+++ сжатие++/Huffman.h
@@ -6,24 +6,26 @@ using namespace std;
 
 struct Node
 {
-	unsigned char symb;
-	unsigned int freq;
-	Node* left, * right;
+    unsigned char symb;
+    unsigned int freq;
+    Node* left, * right;
 };
 
 struct comp
 {
-	bool operator()(Node* l, Node* r)
-	{
-		return l->freq > r->freq;
-	}
+    bool operator()(Node* l, Node* r)
+    {
+        return l->freq > r->freq;
+    }
 };
 
 Node* createNode(unsigned char symb, unsigned int freq, Node* left, Node* right);
 Node* createTree(const int freq[]);
+
 void encode(Node* root, string str, string sCodes[]);
 string encode_string(const vector<unsigned char>& givenStr, const string sCodes[]);
-string bit_string2encode_text(string encode_string_,int& tail_);
+string bit_string2encode_text(string encode_string_, int& tail_);
+
 string symb_bit(int lenght_, unsigned symb_);
-string encode_text2bit_string(string encode_text_);
+string encode_text2bit_string(string encode_text_, int tail_);
 string bit_string2decode_text(string bit_string_, Node* root_);
